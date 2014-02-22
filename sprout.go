17395eaab80812692265032f3d15b3091ca902ed
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-02-21 22:09:53 -0800

    Add skeletal commands, exploration

--- sprout.go
+++ sprout.go
@@ -9,8 +9,26 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "sprout"
 	app.Usage = "Automatically configure of your Mac"
-	app.Action = func(c *cli.Context) {
-		println("Coming soon: much awesomeness.")
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "init",
+			Usage: "Creates the various files that sprout needs in the current directory",
+			Action: func(c *cli.Context) {
+				println("TODO: Create .gitignore")
+				println("TODO: Create site-cookbooks/ directory")
+				println("TODO: Create site-cookbooks/example cookbook")
+				println("TODO: Create Sproutfile")
+			},
+		},
+		{
+			Name:  "run",
+			Usage: "Run's the specified recipe",
+			Action: func(c *cli.Context) {
+				println("TODO: Run pre-requisites (e.g. librarian-chef/berkshelf)")
+				println("TODO: Run recipe with chef-solo: ", c.Args().First())
+			},
+		},
 	}
 
 	app.Run(os.Args)
